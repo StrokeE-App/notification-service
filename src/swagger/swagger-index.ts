1a0e0d419a6263f441120ad855058d28ebe8c732
Carlos29ACC <carlos.caceres@e-hps.com>
2025-02-26 13:41:06 -0500

    added swagger

---
+++ src/swagger/swagger-index.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import { readFileSync } from "fs";
+import yaml from "js-yaml";
+
+const loadSwaggerFile = (filename: string) => {
+  const filePath = path.join(__dirname, filename);
+  const fileContent = readFileSync(filePath, "utf-8");
+  return yaml.load(fileContent);
+};
+
+const chanels = loadSwaggerFile("realTimeChannels.yaml") as any;
+
+const swaggerDocs = {
+  openapi: "3.0.0",
+  info: {
+    title: "StrokeE back-end API",
+    version: "1.0.0",
+    description: "Documentación de la API",
+  },
+  servers: [
+    {
+      url: "http://localhost:4000", 
+    },
+  ],
+  paths: {
+    ...chanels.paths
+
+  },
+};
+
+export default swaggerDocs;
